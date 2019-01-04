a3d8107a99f4e68b5d935f53862e48344e5f3af3
elizathescheisse <elizathescheisse@users.noreply.github.com>
2019-01-04 11:52:19 -0500

    Added log in/log out functionality

--- App.js
+++ App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, TextInput } from 'react-native';
 import Input from './Components/Input/Input';
 import CardSwipe from './Components/CardSwipe/CardSwipe';
 import { f, auth, database } from './config/config.js';
 
 export default class App extends React.Component {
 
-  state = {
-    value: '5',
-    jokes: []
-  }
-
   constructor(props) {
     super(props);
-    this.registerUser('testemailaddress@gmail.com', 'fakepassword');
+    // this.registerUser('testemail@gmail.com', 'fakepassword');
+
+    this.state = {
+      value: '5',
+      jokes: [],
+      loggedin: false
+    };
+
+    var that = this;
+
+    f.auth().onAuthStateChanged(function(user) {
+      if(user) {
+        that.setState({
+          loggedin: true
+        });
+        console.log('Logged in', user);
+      }
+      else {
+        that.setState({
+          loggedin: false
+        });
+        console.log('Logged out');
+      }
+    });
+  }
+
+  loginUser = async(email, pass) => {
+    if(email != '' && pass != '') {
+      try {
+        let user = await auth.signInWithEmailAndPassword(email, pass);
+        console.log(user);
+      } catch(err) {
+        console.log(error);
+      }
+    } else {
+      alert('Missing email or password');
+    }
   }
 
   registerUser = (email, password) => {
     console.log(email, password);
+    // this createUser also logs the user in
     auth.createUserWithEmailAndPassword(email, password)
     .then((userObj) => console.log(email, password, userObj))
     .catch((err) => console.log('error logging in: ', err));
+  }
+
+  signUserOut = () => {
+    auth.signOut()
+    .then(() => {
+      console.log('Logged out...')
+    }).catch((err) => {
+      console.log('Error: ', err);
+    });
   }
 
   render() {
@@ -37,6 +78,53 @@ export default class App extends React.Component {
           updateJokes={(jokes) => this.setState({ jokes: jokes })}
           jokes = {this.state.jokes}
         />
+
+        { this.state.loggedin == true ? (
+          <View>
+            <TouchableHighlight
+            onPress={ () => this.signUserOut() }
+            style={{backgroundColor: 'red'}}>
+              <Text>Log Out</Text>
+            </TouchableHighlight>
+            <Text>Logged in!!!</Text>
+          </View>
+        ) : (
+          <View>
+            { this.state.emailloginView == true ? (
+              <View>
+                <Text>Email: </Text>
+                <TextInput
+                  onChangeText={(text) => this.setState({email: text})}
+                  value={this.state.email}
+                />
+
+                <Text>Password: </Text>
+                <TextInput
+                  onChangeText={(text) => this.setState({pass: text})}
+                  secureTextEntry={true}
+                  value={this.state.pass}
+                />
+
+                <TouchableHighlight
+                  onPress={ () => this.loginUser(this.state.email, this.state.pass) }
+                  style={{backgroundColor: 'blue'}}>
+                    <Text>Login</Text>
+                </TouchableHighlight>
+
+              </View>
+            ) : (
+              <View></View>
+            )}
+
+            <TouchableHighlight
+            onPress={() => this.setState({emailloginView: true})}
+            style={{backgroundColor: 'green'}}>
+              <Text style={{color:'white'}}>Login</Text>
+            </TouchableHighlight>
+
+            <Text>Logged out :/</Text>
+          </View>
+        )}
       </View>
     );
   }
